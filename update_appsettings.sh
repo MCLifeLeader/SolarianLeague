458a650cd77c5286180ae9028682a7abdcc40ac4
Michael Carey <Michael@AGameEmpowerment.com>
2024-04-21 22:42:12 -0600

    Update appsettings.json with new BASE_URL and API_KEY

--- update_appsettings.sh
+++ update_appsettings.sh
@@ -5,5 +5,5 @@ BASE_URL=$1
 API_KEY=$2
 
 # Use sed to replace the placeholders in appsettings.json
-sed -i "s|http://localhost:7138/|$BASE_URL|g" appsettings.json
-sed -i "s|zzyzzx|$API_KEY|g" appsettings.json
+sed -i "s|http://localhost:7138/|$BASE_URL|g" Src/Solarian.League.Web/Solarian.League.Web/wwwroot/appsettings.json
+sed -i "s|zzyzzx|$API_KEY|g" Src/Solarian.League.Web/Solarian.League.Web/wwwroot/appsettings.json
