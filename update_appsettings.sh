a2453b7c1c66303a4d5568a7fb1ca3e3077e3790
Michael Carey <michael@AGameEmpowerment.com>
2024-04-22 21:09:55 -0600

    Fix image paths and update appsettings script

--- update_appsettings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the environment variables
-BASE_URL=$1
-API_KEY=$2
-
-# Use sed to replace the placeholders in appsettings.json
-sed -i "s|http://localhost:7138/|$BASE_URL|g" Src/Solarian.League.Web/Solarian.League.Web/wwwroot/appsettings.json
-sed -i "s|zzyzzx|$API_KEY|g" Src/Solarian.League.Web/Solarian.League.Web/wwwroot/appsettings.json
