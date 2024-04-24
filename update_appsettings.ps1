2a6796f4e69ac1ec24d8f11b00d0bd327ddcb33f
Michael Carey <michael@AGameEmpowerment.com>
2024-04-23 21:38:55 -0600

    Missed transform signature change.

--- update_appsettings.ps1
+++ update_appsettings.ps1
@@ -7,7 +7,7 @@ $appSettings = Get-Content -Path .\Src\Solarian.League.Web\Solarian.League.Web\w
 
 # Use -replace to replace the placeholders in appsettings.json
 $appSettings = $appSettings -replace "http://localhost:7138/", $BASE_URL
-$appSettings = $appSettings -replace "zzyzzx", $API_KEY
+$appSettings = $appSettings -replace "zzyzx", $API_KEY
 
 # Write the updated appsettings.json back to the file
 $appSettings | Set-Content -Path .\Src\Solarian.League.Web\Solarian.League.Web\wwwroot\appsettings.json
