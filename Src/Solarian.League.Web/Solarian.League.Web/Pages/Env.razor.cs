f20d980a8560c15a791238789017481dab3a1dca
Michael Carey <michael@AGameEmpowerment.com>
2024-04-26 19:46:14 -0600

    Added a simple privacy and terms of use page. Updated some additional site text.

--- Src/Solarian.League.Web/Solarian.League.Web/Pages/Env.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Solarian.League.Web.Pages;
-
-public partial class Env
-{
-    protected override async Task OnInitializedAsync()
-    {
-        await base.OnInitializedAsync();
-    }
-}
